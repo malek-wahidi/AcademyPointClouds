83fc736c64be00ee1053ecb1d03ce1bf30188ee1
Mohammad Jouni <mohammad.jouni01@lau.edu>
2025-08-06 22:48:20 +0300

    First working method

--- registration.py
+++ registration.py
@@ -1,12 +1,75 @@
 import open3d as o3d
 import numpy as np
+import copy
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4559,
+                                      front=[0.6452, -0.3036, -0.7011],
+                                      lookat=[1.9892, 2.0208, 1.8945],
+                                      up=[-0.2779, -0.9482, 0.1556])
+    
+
+def preprocess_point_cloud(pcd, voxel_size):
+        pcd_down = pcd.voxel_down_sample(voxel_size)
+
+        radius_normal = voxel_size * 2
+        pcd_down.estimate_normals(
+            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+        radius_feature = voxel_size * 5
+        pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+            pcd_down,
+            o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+        return pcd_down, pcd_fpfh
+
+
+def execute_global_registration(pcd1_down, pcd2_down, pcd1_fpfh,
+                                pcd2_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        pcd1_down, pcd2_down, pcd1_fpfh, pcd2_fpfh, True,
+        distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        3, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.9),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
+    return result
+
+
+def refine_registration(source, target, source_fpfh, target_fpfh, result_ransac, voxel_size):
+    distance_threshold = voxel_size * 0.4
+    result = o3d.pipelines.registration.registration_icp(
+        source, target, distance_threshold, result_ransac.transformation,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
+    return result
+
 
 def register(pcd1: o3d.geometry.PointCloud, pcd2: o3d.geometry.PointCloud) -> np.ndarray:
-    """
-    This is the function that students will implement.
-    It should take two open3d.geometry.PointCloud objects as input
-    and return a 4x4 numpy array representing the transformation matrix
-    that aligns pcd2 to pcd1.
-    """
-    # Placeholder implementation: returns an identity matrix
-    return np.identity(4)
+
+    voxel_size = 0.05
+    
+    pcd1_down, pcd1_fpfh = preprocess_point_cloud(pcd1, voxel_size)
+    pcd2_down, pcd2_fpfh = preprocess_point_cloud(pcd2, voxel_size)
+
+    result_ransac = execute_global_registration(pcd1_down, pcd2_down,
+                                            pcd1_fpfh, pcd2_fpfh,
+                                            voxel_size)
+
+    # draw_registration_result(pcd1_down, pcd2_down, result_ransac.transformation)
+
+    result_icp = refine_registration(pcd1, pcd2, pcd1_fpfh, pcd2_fpfh, result_ransac,
+                                 voxel_size)
+
+    # draw_registration_result(pcd1, pcd2, result_icp.transformation)
+    
+
+    return result_icp.transformation
