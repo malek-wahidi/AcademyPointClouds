b160e7ca14f9e4b2d8d2b8c438eae10e312afd26
Charbel_Jbeily <charbeljbeily17@gmail.com>
2025-08-06 12:56:12 +0300

    Added FPFh Feature Computation in my preprocessing function

--- registration.py
+++ registration.py
@@ -1,25 +1,35 @@
 import open3d as o3d
 import numpy as np
 
-# Step 1: Preprocess the point clouds using Voxel Downsampling and Normal Estimation
+# Step 1: Preprocess the point clouds using Voxel Downsampling, Normal Estimation and FPFH Feature Computation
 # This function will be used to prepare the point clouds for registration.
-def preprocess_point_cloud(pcd: o3d.geometry.PointCloud, voxel_size: float) -> o3d.geometry.PointCloud:
+def preprocess_point_cloud(pcd: o3d.geometry.PointCloud, voxel_size: float):
     print(f"[INFO] Downsampling point cloud with voxel size: {voxel_size}")
     pcd_down = pcd.voxel_down_sample(voxel_size)
 
     print(f"[INFO] Estimating normals with radius: {voxel_size * 2}")
     pcd_down.estimate_normals(
         search_param=o3d.geometry.KDTreeSearchParamHybrid(
-            radius=voxel_size * 2, max_nn=30
+            radius=voxel_size * 2,
+            max_nn=30
         )
     )
-    
-    return pcd_down
+
+    print(f"[INFO] Computing FPFH features with radius: {voxel_size * 5}")
+    fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(
+            radius=voxel_size * 5,
+            max_nn=100
+        )
+    )
+
+    return pcd_down, fpfh
+
 
 def register(pcd1: o3d.geometry.PointCloud, pcd2: o3d.geometry.PointCloud) -> np.ndarray:
-    downpcd1 = preprocess_point_cloud(pcd1, voxel_size=0.05)
-    downpcd2 = preprocess_point_cloud(pcd2, voxel_size=0.05)
+    downpcd1, fpfh1 = preprocess_point_cloud(pcd1, voxel_size=0.05)
+    downpcd2, fpfh2 = preprocess_point_cloud(pcd2, voxel_size=0.05)
 
-    
 
     return np.identity(4)
